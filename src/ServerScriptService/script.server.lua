953e08ff15427f9764137b71cd3eb017b58b02ee
LordMerc <branden.michael.boyd@gmail.com>
2021-03-02 19:26:22 -0500

    Update

--- src/ServerScriptService/script.server.lua
+++ src/ServerScriptService/script.server.lua
@@ -1,18 +1,29 @@
-local HoloService = require(script.HoloService)
+local HoloService = require(game.ReplicatedStorage.Modules.HoloService)
 local Players = game:GetService('Players')
+HoloService:init()
 local function PlayerJoined(Player)
-	local CallCheck = HoloService:GetCalls()
-	local CreateCall = HoloService:new("Operation Cinder")
-	if #CallCheck > 0 then
-		CallCheck[1]:AddMember(Player)
-        print('hti t')
-		print(#CallCheck[1].Members)
-		print('hi')
-        print('hi there?')
-	else
-		CreateCall:AddMember(Player)
-		print(#CreateCall.Members)
-	end
+	Player:SetAttribute('Call_Status','None')
+	print(Player,'joined')
+	local Call = HoloService:newCall()
+	Call:on('ready',function()
+		print('Call is ready')
+		local try = Call:addMember(Player)
+		if not try then
+			print('Person in call?')
+		end
+	end)
+	Call:on('disband',function()
+		local mat = (tick()-Call.created)
+		print('Call lasted',math.floor(mat),'seconds')
+	end)
+	Call:on('newMessage',function(Data)
+		print(Data)
+		if (Data.message == "disband") then
+			Call:Disband()
+		end
+	end)
 end
-
+for i,v in next, Players:GetChildren() do
+	PlayerJoined(v)
+end
 Players.PlayerAdded:Connect(PlayerJoined)
